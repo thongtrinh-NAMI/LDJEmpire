ddbd25aa0a14c16e37e09e4101b14590972c65e6
Thong <thong.trinh@gmail.com>
2026-09-12 18:00:32 -0400

    Add XML sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://ldj-empire.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ];
+}
+
